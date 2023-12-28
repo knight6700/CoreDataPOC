ff42bf13881727478ca134af7142d43accbfdc19
Mahmoud Fares <knight6700@gmail.com>
2023-12-28 17:49:44 +0200

    Initial Commit

---
+++ CoreDataPOC/CoreDataPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPOCApp.swift
+//  CoreDataPOC
+//
+//  Created by MahmoudFares on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
